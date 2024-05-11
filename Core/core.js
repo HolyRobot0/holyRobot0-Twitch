9a7995b02ac44e0e3b7db50195284cb8c720ba44
bbricard <baptiste.bricard@etudiant.univ-rennes1.fr>
2024-05-11 02:38:06 +0200

    teste du module tmi.js

--- Core/core.js
+++ Core/core.js
@@ -1,1 +1,25 @@
 //The core of HolyRobot0
+console.log("C'EST OK ????????")
+const { BOTNAME, SECRET_TOKEN } = require('./auth.js');
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: BOTNAME,
+		password: SECRET_TOKEN
+	},
+	channels: [ 'BrokHoly' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
