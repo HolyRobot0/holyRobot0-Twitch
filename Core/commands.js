2e22e705e546ac640ca5040834205b1fb2bbca0e
bbricard <baptiste.bricard@etudiant.univ-rennes1.fr>
2024-05-11 14:44:15 +0200

    Ajout de paramètres optinelles pour les commandes

--- Core/commands.js
+++ Core/commands.js
@@ -1,7 +1,8 @@
 class Commands {
 
     commands = {
-        '!hello' : require('./commands/hello.js')
+        '!hello' : require('./commands/hello.js'),
+        '!coucou' : require('./commands/coucou.js')
     }
 
 
@@ -9,9 +10,8 @@ class Commands {
         return this.commands.hasOwnProperty(command);
     }
 
-    execute(channel,client,command,tags){
-        console.log(tags)
-        this.commands[command].code(channel,client, tags);
+    execute(channel,client,command,tags, param1 = undefined, param2 = undefined){
+        this.commands[command].code(channel,client, tags, param1, param2);
     }
 
 }
